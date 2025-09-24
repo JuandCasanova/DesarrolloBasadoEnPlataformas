5badfbd9bbdd0379acf6163a2fb1ef17d0f94c43
JuandCasanova <juandcasanova18@gmail.com>
2025-09-24 09:49:29 -0500

    se agregaro nuevas entidades y se conectó la base de datos

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const homeRoutes = require('./routes/home.routes');
 const productosRoutes = require('./routes/productos.routes');
 const contactanosRoutes = require('./routes/contactanos.routes');
+const categoriasRoutes = require('./routes/categorias.routes');
+const pedidosRoutes = require('./routes/pedidos.routes');
+const clientesRoutes = require('./routes/clientes.routes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3307;
 
 app.use(express.json());
 
@@ -13,6 +16,10 @@ app.use(express.json());
 app.use('/api/home', homeRoutes);
 app.use('/api/productos', productosRoutes);
 app.use('/api/contactanos', contactanosRoutes);
+app.use('/api/categorias', categoriasRoutes);
+app.use('/api/pedidos', pedidosRoutes);
+app.use('/api/clientes', clientesRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
